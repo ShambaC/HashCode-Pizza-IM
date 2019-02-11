a1c252c753b215a823d5be168ec25e98f2795da7
ShambaC <38806897+ShambaC@users.noreply.github.com>
2019-02-11 17:57:45 +0530

    a little more stuff

--- main.cpp
+++ main.cpp
@@ -9,9 +9,22 @@ unsigned int L;                         //Minimum number of ingredients
 unsigned int H;                        //Maximum number of cells in the slice
 
 int Pizza[1000][1000];             //Legendary Pizza
+int i, j;                                          //Loop Variables
+
+char filename[10];
+
+fstream input_file;
+
 
 int main()
 {
+    cout<<"Welcome to our master Pizza code !!\n";
+    cout<<"\nLet's create the Pizza first :p ";
+    cout<<"\nTell me the input file name : ";
+    cin>>filename;
+
+    input_file.open(filename, ios::in);
+    input_file.seekg(0);
 
     return 0;
 }
