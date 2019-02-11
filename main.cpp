cbd0cff2d817508ac624eb4ba524217f4a4fc8cb
ShambaC <38806897+ShambaC@users.noreply.github.com>
2019-02-11 17:35:55 +0530

    Variable declaration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include<fstream>
 
 using namespace std;
+
+unsigned int R;                         //Number of rows
+unsigned int C;                        //Number of columns
+unsigned int L;                         //Minimum number of ingredients
+unsigned int H;                        //Maximum number of cells in the slice
+
+int Pizza[1000][1000];             //Legendary Pizza
 
 int main()
 {
